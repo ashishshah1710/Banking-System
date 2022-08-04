282b1137267b5c7b7b3f32a7afd09a46ffcd65ea
amdjsfk05 <amdjsfk05@Amdjfs57.iiht.tech>
2022-08-04 10:28:46 +0530

    dds

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -59,9 +59,10 @@ public class Main {
 	public static void main(String[] args) throws ExitException
 	{
 	
-		int i=1;
-		Scanner s=new Scanner (System.in);
+		
 		show_menu();
+		
+		
 		/*while(true)
 		{
 		 show_menu();
